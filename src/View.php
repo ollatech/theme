7a0719606e19cc3da6eeeeb86c77d3e9826b0572
Olla Tech <olla.code@gmail.com>
2018-02-13 20:18:18 +0700

    global update

--- src/View.php
+++ src/View.php
@@ -5,7 +5,7 @@ namespace Olla\Theme;
 final class View {
 	
     public function render(string $format, array $data, string $template = null, array $options = []) {
-       
         return new JsonResponse(['hai' => true]);
     }
+    
 }
