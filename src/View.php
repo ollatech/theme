f8e4108cd42d126001f2fdacddb993644a41a205
Olla Tech <olla.code@gmail.com>
2018-02-13 21:18:37 +0700

    global update

--- src/View.php
+++ src/View.php
@@ -3,10 +3,12 @@ namespace Olla\Theme;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Twig\Environment as TwigEnvironment;
 
 final class View {
 	
     public function render(string $format, array $data, string $operationId) {
+    	print_r($operationId);
         return new JsonResponse(['hai' => true]);
     }
     
