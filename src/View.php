2b55ba55e82880d98dc88056020beb3b11ca64c0
Olla Tech <olla.code@gmail.com>
2018-02-14 21:52:03 +0700

    global update

--- src/View.php
+++ src/View.php
@@ -4,17 +4,19 @@ namespace Olla\Theme;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Twig\Environment as TwigEnvironment;
-use Olla\Theme\Metadata;
+use Olla\Prisma\Metadata;
+
 final class View {
 	
 	protected $design;
+	protected $metadata;
 	
 	public function __construct(Design $design, Metadata $metadata) {
 		$this->design = $design;
+		$this->metadata = $metadata;
 	}
 
     public function render(array $args = [], array $response = []) {
         return new JsonResponse($args);
     }
-
 }
