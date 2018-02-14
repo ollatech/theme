e2dcb11ec48aa77e7b1161a306a152bde9433924
Olla Tech <olla.code@gmail.com>
2018-02-15 03:23:51 +0700

    global update

--- src/Twig/ReactExtension.php
+++ src/Twig/ReactExtension.php
@@ -22,42 +22,12 @@ class ReactExtension extends \Twig_Extension
      *
      * @return string
      */
-    public function reactRenderComponent($componentName, array $options = array())
+    public function render()
     {
-        $props = isset($options['props']) ? $options['props'] : array();
-        $propsArray = is_array($props) ? $props : json_decode($props);
-
         $str = '';
-        $data = array(
-            'component_name' => $componentName,
-            'props' => $propsArray,
-            'dom_id' => 'sfreact-'.uniqid('reactRenderer', true),
-            'trace' => $this->shouldTrace($options),
-        );
-
-
-        if ($this->shouldRenderClientSide($options)) {
-            $str .= $this->renderContext();
-            $str .=  sprintf(
-                '<script type="application/json" class="js-react-on-rails-component" data-component-name="%s" data-dom-id="%s">%s</script>',
-                $data['component_name'],
-                $data['dom_id'],
-                json_encode($data['props'])
-            );
-        }
-        $str .= '<div id="'.$data['dom_id'].'">';
-        if ($this->shouldRenderServerSide($options)) {
-            $rendered = $this->renderer->render(
-                $data['component_name'],
-                json_encode($data['props']),
-                $data['dom_id'],
-                $this->registeredStores,
-                $data['trace']
-            );
-            $str .= $rendered['evaluated'].$rendered['consoleReplay'];
-        }
+        $str .= '<div id="sdfsdfdsf">';
+        $str = 'sdfdsfdsfdsfdf';
         $str .= '</div>';
-
         return $str;
     }
 
